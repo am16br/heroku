17857f20b31f922937202682795eb519fe15331c
am16br <am16br@my.fsu.edu>
2021-05-13 14:27:38 -0400

    n

---
+++ README.md
@@ -0,0 +1,42 @@
+# Boilerplate for a flask based app with sqlalchemy and postgresql, deploy easily on heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+dependencies used in the virtual_env :
+
+`flask flask-sqlalchemy flask-script gunicorn psycopg2 flask-marshmallow requests python-dotenv flask-bcrypt marshmallow-sqlalchemy python-dotenv`
+
+Install the app and environment :
+
+```bash
+git clone https://github.com/gmolveau/flask-sqlalchemy-heroku
+cd flask-sqlalchemy-heroku
+virtualenv venv -p python3
+source venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+Test the app :
+
+```bash
+# if the heroku toolbelt is installed
+heroku local
+
+# else if on unix
+gunicorn wsgi:app
+
+# else if on windows
+python3 wsgi.py
+```
+
+Test the stack (with docker) :
+```bash
+docker-compose up --build
+
+# or in the background with
+docker-compose up --build -d
+```
+
+Schedule a task for free every 10 minutes, every hour, or every day with heroku-scheduler :
+
+![Scheduler](heroku_scheduler_example.png)
---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os, logging
+
+db = SQLAlchemy()
+ma = Marshmallow()
+bcrypt = Bcrypt()
+
+def create_app():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+    load_dotenv(dotenv_path)
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL'
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+
+    # enable logging via gunicorn
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
+
+    from .api_v1 import api as api_v1_blueprint
+    # all the routes of this api will be prefixed with "/api/v1"
+    # ie. POST /api/v1/users/signup
+    app.register_blueprint(api_v1_blueprint, url_prefix='/api/v1')
+
+    return app
